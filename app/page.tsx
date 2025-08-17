431b6043057c946275e84623f1233fb701c9b9cc
Yarny <111127136+YarnyHub@users.noreply.github.com>
2025-08-17 15:16:49 +1000

    feat: add deploy confirmation text to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            . Deployed with Vercel - auto updates on each push.
           </p>
           <Link
             href="/login"
